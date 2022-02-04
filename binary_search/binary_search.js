4042b4d2c46f9f4c7547c3f9e641e2239f973f1b
Evgeniy Rolenko <jeka@rolenko.ru>
2022-02-05 01:00:05 +0300

    binary search 1st release

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -1,8 +1,9 @@
 let binary_search = function (list, item) {
-    let low = 0;
-    let high = list.length - 1;
-    let mid = 0;
-    let guess = 0;
+    let low = 0; //индекс наименьшего элемента массива
+    let high = list.length - 1; //индекс наибольшего элемента массива (%длина массива% -1)
+    let mid; //переменная для хранения индекса среднего элемента в массиве
+    let guess; //переменная для хранения полученного значения из элемента массива, которую будем срванивать с искомым значением
+
     while (low <= high){
         // выводим индекс первого и последнего элементов массива
         // console.log("low = " + low + ", high = " + high);
@@ -19,6 +20,7 @@ let binary_search = function (list, item) {
 
         // проверяем равно ли среднее MID искомому значению ITEM
         if (guess == item) {
+            document.write("Массив для поиска: "+ list + ". Искомое значение: " + guess +  ". Искомый индекс: " + mid + "<br>");
             return  "Массив для поиска: "+ list + ". Искомое значение: " + guess +  ". Искомый индекс: " + mid ;
             // console.log("guess:" + guess + " = mid: " + mid);
         }
@@ -29,7 +31,9 @@ let binary_search = function (list, item) {
         }
 
     }
+    document.write("Массив для поиска: "+ list + ". Искомое значение: " + item +  ". Значение в массиве не найдено.<br>");
     return "Массив для поиска: "+ list + ". Искомое значение: " + item +  ". Значение в массиве не найдено.";
+
 
 
 
@@ -38,3 +42,7 @@ let binary_search = function (list, item) {
 // console.log ("123");
 let my_list_1 = [1,3,5,7,9,11,12,14,15,23,43,99];
 
+binary_search (my_list_1, -1);
+binary_search (my_list_1, 2);
+binary_search (my_list_1, 5);
+binary_search (my_list_1, 12);
